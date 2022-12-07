a467d07e5ba8be166f3e762bc00c510f48cda14c
kallamvenky <kallamvenky@gmail.com>
2022-12-07 12:05:41 +0530

    this is a commit from y

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Holla</h1>
+        <h1>Holla love kallam</h1>
       </header>
     </div>
   );
